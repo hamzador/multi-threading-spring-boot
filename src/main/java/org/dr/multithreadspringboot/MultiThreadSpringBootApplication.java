f7234a0e33af08340a8cc3249fd1bc2846595ace
unknown <hamozadouyry@gmail.com>
2022-12-05 13:06:40 +0100

    multi-thread 1

---
+++ src/main/java/org/dr/multithreadspringboot/MultiThreadSpringBootApplication.java
@@ -0,0 +1,13 @@
+package org.dr.multithreadspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiThreadSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MultiThreadSpringBootApplication.class, args);
+    }
+
+}
